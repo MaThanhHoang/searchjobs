06196172a8d62f2b2694f43934be6cc4fe1a1468
caominhbinh282 <binhka2000@gmail.com>
2021-04-05 15:20:24 +0700

    update register

--- register.php
+++ register.php
@@ -1,29 +1,4 @@
 <?php include 'inc/header.php'; ?>
-<?php
-	if(isset($_POST['submit']) && $_POST['Username'] !='' && $_POST['Email'] !='' && $_POST['Password'] !='' && $_POST['confirmpassword'] !=''){
-		$username = $_POST['Username'];
-		$email = $_POST['Email'];
-		$password = $_POST['Password'];
-		$confirmpassword = $_POST['confirmpassword'];
-		if($password != $confirmpassword){
-			header('Location: http://localhost:81/searchjobs/register.php');
-		}
-		$sql = "SELECT * FROM TAIKHOAN WHERE TAIKHOAN = '$username'";
-		$old = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($old) > 0){
-			echo "Tài khoản đã tồn tại!";
-			header('Location: http://localhost:81/searchjobs/register.php');
-		}
-		else{
-			$insert = "INSERT INTO TAIKHOAN(TAIKHOAN,EMAIL,MATKHAU) VALUES ('$username','$email','$password')";
-			mysqli_query($conn,$insert);
-			header('Location: http://localhost:81/searchjobs/');
-		}
-	}
-	else{
-		header('Location: http://localhost:81/searchjobs/register.php');
-	}
-?>
 <head>
 	<link href="css/register.css" rel="stylesheet" type="text/css" media="all" />
 </head>
@@ -49,3 +24,31 @@
 </div>
 
 <?php include 'inc/footer.php'; ?>
+<?php
+	$kn = mysqli_connect("localhost","root","","a") or die("Không thể kết nối");
+	mysqli_select_db($kn, "a");
+	//
+	mysqli_query($kn,"SET NAMES 'utf8'");
+	if(isset($_POST['submit']) && $_POST['Username'] !='' && $_POST['Email'] !='' && $_POST['Password'] !='' && $_POST['confirmpassword'] !=''){
+		$username = $_POST['Username'];
+		$email = $_POST['Email'];
+		$password = $_POST['Password'];
+		$confirmpassword = $_POST['confirmpassword'];
+		if($password != $confirmpassword){
+			echo "Nhập lại mật khẩu sai!";
+		}
+		$sql = "SELECT * FROM taikhoan WHERE TAIKHOAN = '$username'";
+		$old = mysqli_query($kn,$sql);
+		if(mysqli_num_rows($old) > 0){
+			echo "Tài khoản đã tồn tại!";
+		}
+		else{
+			$insert = "INSERT INTO taikhoan(TAIKHOAN,EMAIL,MATKHAU) VALUES ('$username','$email','$password')";
+			mysqli_query($kn,$insert);
+			header('Location: http://localhost:81/searchjobs/');
+		}
+	}
+	else{
+		header('Location: http://localhost:81/searchjobs/register.php');
+	}
+?>
