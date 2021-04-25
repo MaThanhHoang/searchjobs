ff099af1598b9f651d2b774e7a268ebce891b7ef
caominhbinh282 <binhka2000@gmail.com>
2021-04-25 17:08:17 +0700

    Up Login

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
-﻿<?php include 'inc/header.php' ?>
+﻿<?php 
+  session_start(); 
+  $_SESSION['Login']="";
+?>
+<?php include 'inc/header.php' ?>
 
 <head>
 	<link href="css/register.css" rel="stylesheet" type="text/css" media="all" />
@@ -8,7 +12,7 @@
 	<h1>Đăng nhập</h1>
 	<div class="main-agileinfo">
 		<div class="agileits-top">
-			<form action="processlogin.php" method="post">
+			<form action="#" method="post">
 				<input class="text email" type="email" name="email" placeholder="Email" required="">
 				<input class="text" type="password" name="password" placeholder="Password" required="">
 				<div class="wthree-text">
@@ -20,10 +24,35 @@
 					<div class="clear"> </div>
 				</div>
 				<input type="submit" value="ĐĂNG NHẬP">
+				<?php include 'config/database.php'?>
+<?php
+	$_SESSION['Login'] = @$_POST['email'];
+	$matkhau = @$_POST['password'];
+	//
+	$caulenh ="SELECT * FROM TAIKHOAN WHERE EMAIL = '".$_SESSION['Login']."'";
+	//
+	$kq = mysqli_query($conn,$caulenh);
+	//
+	if(isset($_SESSION['Login']) && isset($matkhau))
+	{
+		if($dong = mysqli_fetch_array($kq))
+		{
+			if($dong['MATKHAU']==$matkhau)
+			{
+				header('Location: ./index.php');
+			}
+			else
+				echo"Sai password";
+		}
+		else
+			echo "Tài khoản chưa tồn tại!";
+	}
+?>
 			</form>
 			<p>Bạn chưa có tài khoản?<a href="register.php"> Đăng kí</a></p>
 		</div>
 	</div>
 </div>
 <?php include 'inc/footer.php'; ?>
+
 
